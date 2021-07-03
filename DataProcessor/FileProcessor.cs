f8b880b7a5f5d8f3ccad25bcebf42651cbdb1386
Qing Ma <q.ma@ncontrol.cloud>
2021-07-03 10:22:22 +0200

    Add project files.

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace DataProcessor
+{
+    internal class FileProcessor
+    {
+        public FileProcessor(string filePath)
+        {
+            InputFilePath = filePath;
+        }
+
+        public string InputFilePath { get; }
+
+        public void Process()
+        {
+            WriteLine($"Begin process of {InputFilePath}");
+        }
+    }
+}
---
+++ DataProcessor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DataProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parsing command line options");
+
+            // Comand line validation omitted for brevity
+
+            var command = args[0];
+
+            if (command == "--file")
+            {
+                var filePath = args[1];
+                Console.WriteLine($"Single file {filePath} selected");
+                ProcessSingleFile(filePath);
+            }
+            else if (command == "--dir")
+            {
+                var directoryPath = args[1];
+                var fileType = args[2];
+                Console.WriteLine($"Directory {directoryPath} selected for {fileType} files");
+                ProcessDirectory(directoryPath, fileType);
+            } else
+            {
+                Console.WriteLine("Invaliid command line options");
+            }
+
+            Console.WriteLine("Press enter to quit.");
+            Console.ReadLine();
+        }
+
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+    }
+}
